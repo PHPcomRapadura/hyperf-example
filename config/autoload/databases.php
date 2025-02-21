f202e2916318488fc9ae071f08ff87c08c18b8e8
William Correa <wilcorrea@gmail.com>
2025-02-21 15:53:32 -0300

    feat: configura conexão com banco de dados

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -2,45 +2,83 @@
 
 declare(strict_types=1);
 
-use Hyperf\ModelCache\Handler\RedisHandler;
-
 use function Hyperf\Support\env;
 
-return [
-    'default' => [
-        'driver' => env('DB_DRIVER', 'mysql'),
-        'host' => env('DB_HOST', 'localhost'),
-        'port' => env('DB_PORT', 3306),
-        'database' => env('DB_DATABASE', 'hyperf'),
-        'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', ''),
-        'charset' => env('DB_CHARSET', 'utf8mb4'),
-        'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+$connections = [
+    'postgres' => [
+        'driver' => 'pgsql',
+        'read' => [
+            'host' => [
+                env('DB_HOST_READER', env('DB_HOST')),
+            ],
+            'username' => env('DB_USERNAME_READER', env('DB_USERNAME', 'root')),
+            'password' => env('DB_PASSWORD_READER', env('DB_PASSWORD', '')),
+        ],
+        'write' => [
+            'host' => [
+                env('DB_HOST'),
+            ],
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+        ],
+        'port' => env('DB_PORT', 5432),
+        'database' => env('DB_DATABASE', 'hyperf-example'),
+        'charset' => env('DB_CHARSET', 'utf8'),
+        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
+        'schema' => env('POSTGRES_SCHEMA', 'public'),
         'pool' => [
             'min_connections' => 1,
-            'max_connections' => 10,
+            'max_connections' => 30,
             'connect_timeout' => 10.0,
             'wait_timeout' => 3.0,
             'heartbeat' => -1,
             'max_idle_time' => (float) env('DB_MAX_IDLE_TIME', 60),
         ],
-        'cache' => [
-            'handler' => RedisHandler::class,
-            'cache_key' => '{mc:%s:m:%s}:%s:%s',
-            'prefix' => 'default',
-            'ttl' => 3600 * 24,
-            'empty_model_ttl' => 600,
-            'load_script' => true,
-        ],
-        'commands' => [
-            'gen:model' => [
-                'path' => 'app/Model',
-                'force_casts' => true,
-                'inheritance' => 'Model',
-                'uses' => '',
-                'table_mapping' => [],
-            ],
+        'options' => [
+            PDO::ATTR_TIMEOUT => 5,
         ],
     ],
+    'mysql' => [
+        'driver' => 'mysql',
+        'read' => [
+            'host' => [
+                env('MY_SQL_DB_HOST_READER', 'mysql'),
+            ],
+            'username' => env('MY_SQL_DB_USERNAME_READER', 'root'),
+            'password' => env('MY_SQL_DB_PASSWORD_READER', 'root'),
+            'port' => env('MY_SQL_DB_PORT_READER', 3306),
+        ],
+        'write' => [
+            'host' => [
+                env('MY_SQL_DB_HOST_WRITE', 'mysql'),
+            ],
+            'username' => env('MY_SQL_DB_USERNAME_WRITE', 'root'),
+            'password' => env('MY_SQL_DB_PASSWORD_WRITE', 'root'),
+            'port' => env('MY_SQL_DB_PORT_WRITE', 3306),
+        ],
+        'database' => env('DB_DATABASE', 'hyperf-example'),
+        'port' => env('MY_SQL_DB_PORT_READER', 3306),
+        'charset' => env('DB_CHARSET', 'utf8'),
+        'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
+        'prefix' => env('DB_PREFIX', ''),
+        'pool' => [
+            'min_connections' => 1,
+            'max_connections' => 30,
+            'connect_timeout' => 10.0,
+            'wait_timeout' => 3.0,
+            'heartbeat' => -1,
+            'max_idle_time' => (float) env('DB_MAX_IDLE_TIME', 60),
+        ],
+        'options' => [
+            PDO::ATTR_TIMEOUT => 5,
+        ],
+    ],
+];
+
+$connection = env('DB_CONNECTION', 'postgres');
+return [
+    'default' => $connection[$connection],
+    'connections' => $connections,
+    'connection' => $connection,
 ];
